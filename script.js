2e9accbf61bd346e1007193ab8c03f8ed0ba7730
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-09-10 10:36:15 +0200

    Added opponent timeout

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", start);
 
 const drawPile = document.querySelector("#drawpile");
 const drawnCard = document.querySelector("#drawn");
-const cardColors = ["blue", "red", "yellow", "green", "brown"];
-const cards = document.querySelectorAll(".handcard");
+const cardColors = ["water", "fire", "air", "plant", "rock"];
+const handCards = document.querySelectorAll(".handcard");
 
+let handCardsNumber = 0;
 let randomCard = "";
+
+let cardsLeft = 20;
 
 function getRandom() {
     randomCard = cardColors[Math.floor(Math.random()*cardColors.length)];
@@ -17,33 +20,55 @@ function start() {
     console.log("Hi");
     drawPile.classList.add("cardback");
     drawPile.addEventListener("click", newCard);
+
+    handCards.forEach(card => {
+        handCardsNumber++;
+        card.classList.add(`handcard${handCardsNumber}`);
+    });
 }
 
-function newCard() {
+function newCard(player) {
+
+    if (player === "opponent") {
+        console.log("YOU LOSE");
+    }
+
     if (randomCard === "") {
+
+        cardsLeft--;
+        console.log(cardsLeft);
 
     getRandom();
 
     cardColors.forEach(color => {
-        drawnCard.classList.remove(`${color}card`);
+        drawnCard.classList.remove(`${color}`);
     });
 
-    drawnCard.classList.add(`${randomCard}card`);
+    drawnCard.classList.add(`${randomCard}`);
 
-    cards.forEach(card => {
+    handCards.forEach(card => {
       card.addEventListener("click", addCardToHand);
     });
     }
 }
 
 function addCardToHand() {
+    if (randomCard !== "") {
     cardColors.forEach(color => {
-        drawnCard.classList.remove(`${color}card`);
-        this.classList.remove(`${color}card`);
+        drawnCard.classList.remove(`${color}`);
+        this.classList.remove(`${color}`);
     });
 
-    this.classList.add(`${randomCard}card`);
-
+    this.classList.add(`${randomCard}`);
 
     randomCard = "";
+
+    opponentDraw();
+    }
+}
+
+function opponentDraw() {
+    setTimeout(function () {
+        newCard("opponent")
+    }, 1000);
 }
